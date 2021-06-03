e2d498c632e34127cdbd9d157f031b948a4c0137
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-06-03 19:46:39 -0300

    criação pagina pedido

---
+++ src/app/pages/pedido/pedido.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido',
+  templateUrl: './pedido.page.html',
+  styleUrls: ['./pedido.page.scss'],
+})
+export class PedidoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
