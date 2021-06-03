17311534822fda70f05c1c4a528276948ea3a255
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-06-03 19:44:07 -0300

    criação pagina endereço

---
+++ src/app/pages/endereco/endereco.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-endereco',
+  templateUrl: './endereco.page.html',
+  styleUrls: ['./endereco.page.scss'],
+})
+export class EnderecoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
