b32e103ec660a662d4117c8827dba2219b62d800
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-06-04 17:44:36 -0300

    implementação login guard

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(private authService: AuthService,
+    private router: Router) { }
+
+  canActivate(): Promise<boolean> {
+    return new Promise(resolve => {
+      this.authService.getAuth().onAuthStateChanged(user => {
+        if (user) this.router.navigate(['/home']);
+        resolve(!user ? true : false);
+      })
+    })
   }
-  
+
 }
