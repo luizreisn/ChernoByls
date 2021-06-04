72f38d6789ed0301809e93c09f953faa372ffbdc
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-06-04 20:34:21 -0300

    implementando interface usuario

--- src/app/interfaces/usuario.ts
+++ src/app/interfaces/usuario.ts
@@ -1,2 +1,17 @@
+import { DadoEndereco } from "./dado-endereco";
+import { Pedido } from "./pedido";
+import { Produto } from "./produto";
+
 export interface Usuario {
+    nome?: string;
+    telefone?: number;
+    cpf?: number;
+    nascimento?: Date;
+    sexo?: string;
+    email?: string;
+    senha?: string;
+    dadosEndereco?: DadoEndereco;
+    produtosFavoritos?: string[];
+    carrinho?: Produto[];
+    pedidos?: Pedido[];
 }
