0bb6189deff6f09cee4cf5032d8591d0a89c669a
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-06-04 20:48:07 -0300

    implementação interface de produto

--- src/app/interfaces/produto.ts
+++ src/app/interfaces/produto.ts
@@ -1,2 +1,14 @@
 export interface Produto {
+    id?: string;
+    categoria?: string;
+    nome?: string;
+    descricao?: string;
+    img?: string;
+    personalizacao?: {
+        nome?: string;
+        marcado?: boolean;
+    }[];
+    valor?: number;
+    quantidade?: number;
+    valorTotal?: number;
 }
