8db24e2285f77d71572c0e49b8b2c5b15aecedcd
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-06-04 21:09:40 -0300

    implementação service de avisos

--- src/app/services/aviso.service.ts
+++ src/app/services/aviso.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Aviso } from '../interfaces/aviso';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AvisoService {
 
-  constructor() { }
+  private avisoColecao: AngularFirestoreCollection<Aviso>;
+
+  constructor(private afs: AngularFirestore) {
+    this.avisoColecao = this.afs.collection<Aviso>('Avisos');
+  }
+
+  public getAvisos() {
+    return this.avisoColecao.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        })
+      })
+    )
+  }
 }
