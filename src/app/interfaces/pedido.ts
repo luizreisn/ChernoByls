119abf4d213054da7722599a4a1023008ab3a9d5
LaraRCrudo <lararomaneli@hotmail.com>
2021-06-05 01:21:52 -0300

    implementação interface pedido

--- src/app/interfaces/pedido.ts
+++ src/app/interfaces/pedido.ts
@@ -1,2 +1,14 @@
+import { Produto } from "./produto";
+import firebase from "firebase/app"
+
 export interface Pedido {
-}
+  id?: string;
+  produtos?: Produto[];
+  subtotal?: number;
+  frete?: number:
+  total?: number;
+  data?: firebase.firestore.Timestamp;
+  endereco?: string;
+  numero?: number;
+}[];
+
