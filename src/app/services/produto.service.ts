76157fedf854c891dadb38781ed33757f9fb45f1
luizreis <luizgabrieldosreisnunes2001@gmail.com>
2021-06-04 21:03:33 -0300

    implementação da service de produtos

--- src/app/services/produto.service.ts
+++ src/app/services/produto.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Produto } from '../interfaces/produto';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProdutoService {
 
-  constructor() { }
+  private produtoColecao: AngularFirestoreCollection<Produto>;
+
+  constructor(private afs: AngularFirestore) {
+    this.produtoColecao = this.afs.collection<Produto>('Produtos');
+  }
+
+  public getProdutos() {
+    return this.produtoColecao.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        })
+      })
+    )
+  }
+
+  public getProduto(id: string) {
+    return this.produtoColecao.doc<Produto>(id).valueChanges();
+  }
 }
