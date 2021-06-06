3ed6fd10a148688a21ba5f99028ff08f462febb2
LaraRCrudo <lararomaneli@hotmail.com>
2021-06-06 00:04:28 -0300

    implementacao service botao menu

--- src/app/services/botao-menu.service.ts
+++ src/app/services/botao-menu.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { BotoesMenu } from '../interfaces/botoes-menu';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BotaoMenuService {
 
-  constructor() { }
+  private botoesMenuColecao: AngularFirestoreCollection<BotoesMenu>;
+
+  constructor(private afs: AngularFirestore) {
+    this.botoesMenuColecao = this.afs.collection<BotoesMenu>('BotoesMenu');
+  }
+
+  public getBotoesMenu() {
+    return this.botoesMenuColecao.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        })
+      })
+    )
+  }
+
 }
