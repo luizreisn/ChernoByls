b8d4e5a5157d77513e8d802516ee09307565eba8
maucanotilho <mau.canotilho@gmail.com>
2021-06-06 17:08:22 -0300

    implementacao menu esp service

--- src/app/services/menu-esp.service.ts
+++ src/app/services/menu-esp.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators';
+import { MenuEspecifico } from '../interfaces/menu-especifico';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MenuEspService {
 
-  constructor() { }
+  private menuEspColecao: AngularFirestoreCollection<MenuEspecifico>;
+
+  constructor(private afs: AngularFirestore) { 
+    this.menuEspColecao = this.afs.collection<MenuEspecifico>('MenusEsp');
+  }
+
+  public getMenusEsp(){
+    return this.menuEspColecao.snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data();
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        })
+      })
+    )
+  }
+
+  public getMenuEsp(id: string){
+    return this.menuEspColecao.doc<MenuEspecifico>(id).valueChanges();
+  }
 }
