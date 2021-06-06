6956159c808e5cb6a370930b2baea78500ae01eb
LaraRCrudo <lararomaneli@hotmail.com>
2021-06-06 00:19:29 -0300

    implementacao pagina pedidos anteriores ts

--- src/app/pages/pedidos-anteriores/pedidos-anteriores.page.ts
+++ src/app/pages/pedidos-anteriores/pedidos-anteriores.page.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Pedido } from '../interfaces/pedido';
+import { Usuario } from '../interfaces/usuario';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-pedidos-anteriores',
   templateUrl: './pedidos-anteriores.page.html',
   styleUrls: ['./pedidos-anteriores.page.scss'],
 })
-export class PedidosAnterioresPage implements OnInit {
+export class PedidosAnterioresPage {
 
-  constructor() { }
+  public quantidadePedidos: number;
+  public data: Pedido[];
 
-  ngOnInit() {
+  public usuario: Usuario = {};
+  public usuarioId: string = null;
+  public usuarioSubscription: Subscription;
+
+  constructor(private authService: AuthService,) { 
+    this.carregarDados();
+  }
+
+  public async carregarDados(){
+    this.usuarioId = (await this.authService.getAuth().currentUser).uid 
+    this.usuarioSubscription = this.authService.getUsuario(this.usuarioId).subscribe(data => {
+      this.usuario = data;
+      this.quantidadePedidos = this.usuario.pedido.length;
+    });
+  }
+
+  ngOnDestroy(){
+    this.usuarioSubscription.unsubscribe();
   }
 
 }
